664b91ecbb30785178553d84378a67198e9ff0fe
Jon9851 <joncohen21k@hotmail.co.uk>
2022-02-21 15:05:29 +0000

    questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,256 +1,154 @@
 let MCQS = [{
-    question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+    question: "what Year was the collosuem finsihed?",
+    choice1:  "80 AD",
+    choice2:  "90 AD",
+    choice3:  "60 AD",
+    choice4:  "50 AD",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "Who was the first emperor for rome?",
+    choice2:  "Augustus",
+    choice1:  "Julius Caes",
+    choice3:  "Ne",
+    choice4:  "Marcus Aureli",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "Whats was the bloodiest battle during the first world war?",
+    choice1:  "Battle of Verdun-",
+    choice2:  "Battle of Somme",
+    choice3:  "Battle of Amiens",
+    choice4:  "Battle of Passchebdaele",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "What year did Queen Elizabeth become queen of United Kingdom?",
+    choice1:  "10th of May 1954",
+    choice2:  "3rd of August 1949",
+    choice3:  "2nd of June 1953 -",
+    choice4:  "23rd of july 1951",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "which country gave the statue of liberty a gift to the united states?",
+    choice1:  "Germany",
+    choice2:  "Spain",
+    choice3:  "United Kingdom",
+    choice4:  "France-",
+    answer: 4
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "What was the Largest Contiguous Empire in History?",
+    choice1:  "Roman Empire",
+    choice2:  "British Empire",
+    choice3:  "Mongolian Empire-",
+    choice4:  "Ottoman Empire",
+    answer: 3
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "When was the Declaration of Independence Signed?",
+    choice1:  "July 2nd 1776 -",
+    choice2:  "March 10th 1776",
+    choice3:  "August 21st 1776",
+    choice4:  "September 1st 1776",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "Who was the first president of the United States?",
+    choice1:  "George Washington -",
+    choice2:  "Abraham Lincoln",
+    choice3:  "John Adams ",
+    choice4:  "Thomas Jefferson",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ]  question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ]  question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
+{
+    question: "",
+    choice1:  "",
+    choice2:  "",
+    choice3:  "",
+    choice4:  "",
+    answer: 1
 },
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
-                question: "";
-    choice1:  "";
-    choice2:  "";
-    choice3:  "";
-    choice4:  "";
-    answer:  
-},
-               ] 
 
+];
