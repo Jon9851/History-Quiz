6d317d8888ea9d5c59e649ceaf823ff61152ceb4
Jon9851 <joncohen21k@hotmail.co.uk>
2022-03-04 02:03:50 +0000

    socre.html link via java

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,7 +168,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/score.html')
+        return window.location.assign('score.html')
     }
 
     questionCounter++
